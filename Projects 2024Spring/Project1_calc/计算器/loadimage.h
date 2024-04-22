1caf4e1f2528e8d932b8e3b3d1724098232fb5bf
EulerDanseur <linhenry980@gmail.com>
2024-04-22 22:29:35 +0800

    20240422

--- Projects 2024Spring/Project1_calc/计算器/loadimage.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<easyx.h>
-#define MyChar TCHAR
-
-extern MyChar Consolas[];
-IMAGE setimage(int wigth, int height, COLORREF rgb);	// 指定绘图设备
--- Projects 2024Spring/test.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include <graphics.h>
-using namespace std;
-
-int main()
-{
-    cout << "Hello World" << endl;
-    return 0;
-}
